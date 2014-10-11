923a92a51f96b81b0f3a261dade1c08fccf77c7d
mike <mike@Mac-Pro-de-mike.local>
2014-10-11 14:42:59 -0700

    Initial Commit

---
+++ Pract3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pract3
+//
+//  Created by mike on 11/10/14.
+//  Copyright (c) 2014 mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
